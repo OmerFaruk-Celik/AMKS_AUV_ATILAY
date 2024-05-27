7b76b7ed472ed93ee6a1f0e6729a0070b6238970
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-05-27 22:45:01 +0300

    python oto commit

--- examples/demo4.py
+++ examples/demo4.py
@@ -129,8 +129,8 @@ def update_frame(frame):
     # Veriyi güncelle
     line.set_ydata(filtered_data)
     if frekans_peak >lowcut  and frekans_peak<highcut:
-        print(genis_veri[:16])
-        #print(al(genis_veri))
+        #print(genis_veri[:19])
+        print(al(genis_veri))
     
     # Frekans değerini güncelle
     text.set_text(f'Frekans: {frekans_peak:.2f} Hz')
