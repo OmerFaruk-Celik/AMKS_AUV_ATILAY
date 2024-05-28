f2818d6a96f392b2902724b201ce679082be2dd2
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-05-28 17:57:01 +0300

    oto commit 2024-05-28 17:57:01.943583

--- examples/demo4.py
+++ examples/demo4.py
@@ -105,7 +105,7 @@ fig, ax = plt.subplots()
 x = np.arange(0, 2 * CHUNK, 2)
 line, = ax.plot(x, np.random.rand(CHUNK))
 
-ax.set_ylim(-2000, 2000)
+ax.set_ylim(-10, 10)
 ax.set_xlim(0, CHUNK)
 plt.xlabel('Zaman')
 plt.ylabel('Genlik')
@@ -148,7 +148,7 @@ def update_frame(frame):
         #genis_veri = np.where(filtered_data <= 0, -1, 1)
 
         # Veriyi güncelle
-        line.set_ydata(filtered_data)
+        line.set_ydata(genis_veri)
 
         # Frekans değerini güncelle
         text.set_text(f'Frekans: {frekans_peak:.2f} Hz')
