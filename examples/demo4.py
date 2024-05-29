7f4689d234d659a8dc6e9e6d1f2e035e98b14b6a
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-05-30 02:33:01 +0300

    oto commit 2024-05-30 02:33:01.250851

--- examples/demo4.py
+++ examples/demo4.py
@@ -166,6 +166,7 @@ def update_frame(frame):
     frekans = np.fft.rfftfreq(len(data_int), 1/RATE)
     spektrum = np.fft.rfft(data_int)
     frekans_peak = frekans[np.argmax(np.abs(spektrum))]
+    print(frekans_peak)
 
 
     # Eğer frekans koşulu sağlanıyorsa:
