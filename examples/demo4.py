2af2c295b883cd7cf978a470a3040d55f0daa6dd
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-05-30 02:07:01 +0300

    oto commit 2024-05-30 02:07:01.490422

--- examples/demo4.py
+++ examples/demo4.py
@@ -176,7 +176,7 @@ def update_frame(frame):
         b = [1.0 / n] * n
         a = 1
         yy = lfilter(b, a, data_int)
-        filtered_data=yy
+        filtered_data=data_int
         
         
 
