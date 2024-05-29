8e6a72efabbfaecb2fa81375d693656d89ed22e3
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-05-30 02:02:01 +0300

    oto commit 2024-05-30 02:02:01.891452

--- examples/demo4.py
+++ examples/demo4.py
@@ -84,8 +84,8 @@ def filtrele(data, esik):
     return filtered_data
 
 # Frekans aralığı
-lowcut = 1000.0
-highcut = 22000.0
+lowcut = 15000.0
+highcut = 19000.0
 
 # Grafik hazırlıkları
 fig, ax = plt.subplots()
