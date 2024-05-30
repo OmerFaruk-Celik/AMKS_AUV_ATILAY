ceea2d8a837f4157d85fa26031e4783ed64c8dfb
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-05-30 15:15:02 +0300

    oto commit 2024-05-30 15:15:01.979355

--- examples/demo4.py
+++ examples/demo4.py
@@ -134,8 +134,8 @@ def parca_kontrol(s, sutun_sayisi, rate):
   ortalama_frekans=ortalama_frekans/sutun_sayisi
   frekanslar=np.array(frekanslar)
   print(frekanslar)
-  kosul1=frekanslar>17910
-  kosul2=frekanslar<17920
+  kosul1=frekanslar>19290
+  kosul2=frekanslar<19296
   sonuc=kosul1 & kosul2
   
   bits=np.where(sonuc,-1,1)
