e2bbd4eaa9c13b169765195d381d0e54d9c44c62
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-08-07 22:24:01 +0300

    oto commit 2024-08-07 22:24:01.407244

--- examples/frekansYakala2.py
+++ examples/frekansYakala2.py
@@ -8,7 +8,7 @@ from scipy.signal import butter, lfilter
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-CHUNK = 1024 * 20
+CHUNK = 1024 * 10
 
 # Initialize audio stream
 p = pyaudio.PyAudio()
@@ -66,7 +66,7 @@ fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 10))
 # Time domain plot
 line, = ax1.plot(np.arange(CHUNK), np.zeros(CHUNK))
 ax1.set_ylim(-32768, 32767)
-ax1.set_xlim(80, CHUNK/200)
+ax1.set_xlim(0, CHUNK/100)
 ax1.set_title("Time Domain")
 ax1.set_xlabel("Samples")
 ax1.set_ylabel("Amplitude")
