c5d9487cc80940d9a73aab3484f266a04aab51ee
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-08-07 21:49:01 +0300

    oto commit 2024-08-07 21:49:01.780379

--- examples/frekansYakala.py
+++ examples/frekansYakala.py
@@ -32,8 +32,8 @@ def bandpass_filter(data, lowcut, highcut, fs, order=5):
     return y
 
 # Filter parameters
-lowcut = 16000.0
-highcut = 17000.0
+lowcut = 16600.0
+highcut = 16630.0
 
 # Function to update frame
 def update_frame(i):
