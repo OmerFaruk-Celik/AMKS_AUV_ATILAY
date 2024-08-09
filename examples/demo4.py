08f244b10c7fcd2dab5eb1521b67d139b673a516
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2024-08-09 18:02:01 +0300

    oto commit 2024-08-09 18:02:01.471238

--- examples/demo4.py
+++ examples/demo4.py
@@ -21,22 +21,30 @@ stream = p.open(
 )
 
 # Grafik hazırlıkları
-fig, ax = plt.subplots()
+fig, (ax1, ax2) = plt.subplots(2, 1)
+
+# Zaman aralığını hesapla
 x = np.arange(0, CHUNK) * (1.0 / RATE)  # Zaman ekseni
-line, = ax.plot(x, np.random.rand(CHUNK))
+line1, = ax1.plot(x, np.random.rand(CHUNK))
+line2, = ax2.plot([], [])  # İkinci grafik için boş başlangıç
 
-ax.set_ylim(-32000, 32000)
-ax.set_xlim(0, CHUNK * (1.0 / RATE))  # X ekseni için zamansal ölçek
+ax1.set_ylim(-32000, 32000)
+ax1.set_xlim(0, CHUNK * (1.0 / RATE))  # X ekseni için zamansal ölçek
+ax1.set_xlabel('Zaman (saniye)')
+ax1.set_ylabel('Genlik')
+ax1.set_title('Osiloskop')
 
-plt.xlabel('Zaman (saniye)')
-plt.ylabel('Genlik')
-plt.title('Osiloskop')
+ax2.set_ylim(-32000, 32000)
+ax2.set_xlim(0, 10 * (1.0 / RATE))  # İkinci grafik için X eksenini ayarlayın (10 dalga boyu)
+ax2.set_xlabel('Zaman (saniye)')
+ax2.set_ylabel('Genlik')
+ax2.set_title('10 Dalga Boyu')
 
 # Frekans değeri için metin ekleyin
-text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+text = ax1.text(0.05, 0.9, '', transform=ax1.transAxes)
 
 # Dalga boyu sayısı için metin ekleyin
-wavelength_text = ax.text(0.05, 0.8, '', transform=ax.transAxes)
+wavelength_text = ax1.text(0.05, 0.8, '', transform=ax1.transAxes)
 
 def update_frame(frame):
     data = stream.read(CHUNK, exception_on_overflow=False)
@@ -54,7 +62,7 @@ def update_frame(frame):
         a = 1
         yy = lfilter(b, a, data_int)
 
-        line.set_ydata(yy)
+        line1.set_ydata(yy)
 
         # Frekans değerini güncelle
         text.set_text(f'Frekans: {frekans_peak:.2f} Hz')
@@ -64,12 +72,22 @@ def update_frame(frame):
         number_of_wavelengths = frekans_peak * total_time
         wavelength_text.set_text(f'Dalga Boyu Sayısı: {number_of_wavelengths:.2f}')
         
+        # 10 Dalga Boyunu Hesapla
+        samples_per_wave = RATE / frekans_peak  # Bir dalga boyu için örnek sayısı
+        total_samples = int(10 * samples_per_wave)  # 10 dalga boyu için toplam örnek sayısı
+
+        if total_samples <= len(yy):
+            wave_data = yy[:total_samples]
+            wave_x = np.arange(0, total_samples) * (1.0 / RATE)
+            line2.set_data(wave_x, wave_data)
+            ax2.set_xlim(0, wave_x[-1])
+
     else:
-        line.set_ydata(np.repeat(0, CHUNK))
+        line1.set_ydata(np.repeat(0, CHUNK))
         text.set_text('Frekans: Dışı')
         wavelength_text.set_text('Dalga Boyu Sayısı: -')
 
-    return line, text, wavelength_text
+    return line1, line2, text, wavelength_text
 
 # Animasyonu başlat
 ani = animation.FuncAnimation(fig, update_frame, interval=5, blit=True)
