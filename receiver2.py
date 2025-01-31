d25c822d52fb4414b1689289ec6439c0c12ea66c
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-01 01:19:01 +0300

    oto commit 2025-02-01 01:19:01.979935

--- receiver2.py
+++ receiver2.py
@@ -71,7 +71,7 @@ def process_audio():
     """Bu fonksiyon kuyruktaki ses verilerini alır ve band geçiren filtre uygular."""
     while True:
         if not q.empty():
-            print(len(list(q.queue)))
+            #print(len(list(q.queue)))
             indata = q.get()
             # 19 kHz band geçiren filtre
             filtered_19kHz = bandpass_filter(indata[:, 0], 18000, 19500, sampling_rate)
@@ -80,7 +80,7 @@ def process_audio():
             # 15 kHz band geçiren filtre
             filtered_15kHz = bandpass_filter(indata[:, 0], 14500, 15500, sampling_rate)
             signal_15kHz = detect_signal(filtered_15kHz)
-            
+            print(f"19 kHz Signal: {'1' if signal_19kHz else '0'}, 15 kHz Signal: {'1' if signal_15kHz else '0'}")
             xor_or(signal_19kHz, signal_15kHz)
             #print(list(q2.queue))
 
