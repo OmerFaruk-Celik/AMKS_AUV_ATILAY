cd5581bce29f57f59f8a44f8d8a7cc0d1947b9f4
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-01-31 17:14:01 +0300

    oto commit 2025-01-31 17:14:01.191000

--- sim3D.py
+++ sim3D.py
@@ -9,7 +9,7 @@ x, y = np.meshgrid(x, y)
 z = np.sin(np.sqrt(x**2 + y**2))
 
 # Büyük boyutlu 3D grafik oluşturma
-fig = plt.figure(figsize=(8, 4))  # Figür boyutlarını ayarlama (inch cinsinden)
+fig = plt.figure(figsize=(16, 9))  # Figür boyutlarını ayarlama (inch cinsinden)
 ax = fig.add_subplot(111, projection='3d')
 
 # 3D yüzey çizimi
@@ -22,9 +22,9 @@ ax.set_zlabel('Z Eksen')
 ax.set_title('Tüm Ekranı Kaplayan 3D Grafik Örneği')
 
 # Eksen sınırlarını genişletme
-ax.set_xlim([-10, 10])
-ax.set_ylim([-10, 10])
-ax.set_zlim([-1, 1])
+ax.set_xlim([-20, 20])
+ax.set_ylim([-20, 20])
+ax.set_zlim([-2, 2])
 
 # Grafik gösterimi
 plt.show()
