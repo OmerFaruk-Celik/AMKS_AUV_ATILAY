b748b6d424251a6e2b47d4eb2b0d858364615003
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-01 13:24:01 +0300

    oto commit 2025-02-01 13:24:01.574254

--- receiver3.py
+++ receiver3.py
@@ -3,16 +3,15 @@ import sounddevice as sd
 from scipy.signal import butter, lfilter
 import queue
 import threading
-import matplotlib.pyplot as plt
 
 # Sabitler
 sampling_rate = 40000  # Örnekleme frekansı (Hz)
-block_duration = 0.001  # Blok süresi (saniye) - 1 ms
-blocksize = int(sampling_rate * block_duration)  # Blok boyutu (örnek sayısı)
+block_duration = 0.00025  # Blok süresi (saniye) - 0.25 ms
+blocksize = int(sampling_rate * block_duration)  # Blok boyutu (örnek sayısı) = 10
 scale_factor = 10  # Genlik ölçekleme faktörü
 
 # Ses verilerini tutmak için bir kuyruk oluşturun
-q = queue.Queue(maxsize=sampling_rate*block_duration )  # Maksimum boyutu belirleyin
+q = queue.Queue(maxsize=blocksize)  # Maksimum boyutu belirleyin
 q2 = queue.Queue(maxsize=16)
 
 def butter_bandpass(lowcut, highcut, fs, order=5):
@@ -81,55 +80,15 @@ def process_audio():
             signal_10kHz = detect_signal(filtered_10kHz)
             #print(f"15 kHz Signal: {'1' if signal_15kHz else '0'}, 10 kHz Signal: {'1' if signal_10kHz else '0'}")
             xor_or(signal_15kHz, signal_10kHz)
-            print(list(q2.queue)) ##Bu yorum satırlarını silme lazım lacak şekilde tekrardan kullanmak için şimdilik yotum satırına alıyorum
-
-def update_plot(fig, ax, line1, line2):
-    """Bu fonksiyon grafiği günceller."""
-    while True:
-        if not q.empty():
-            indata = q.queue[-1]  # Son alınan veriyi kullan
-            # 15 kHz band geçiren filtre
-            filtered_15kHz = bandpass_filter(indata[:, 0], 14500, 15500, sampling_rate)
-            # 10 kHz band geçiren filtre
-            filtered_10kHz = bandpass_filter(indata[:, 0], 9500, 10500, sampling_rate)
-            """
-            # Anlık verileri güncelleme ve grafikte gösterme
-            line1.set_ydata(filtered_15kHz)
-            line2.set_ydata(filtered_10kHz)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
-            """
+            ##printlist(q2.queue)) ##Bu yorum satırlarını silme lazım olacak şekilde tekrardan kullanmak için şimdilik yorum satırına alıyorum
 
 def listen_microphone():
-    """
-    plt.ion()  # Interaktif modu etkinleştir
-    fig, ax = plt.subplots(2, 1)  # İki alt grafik oluştur
-    x = np.linspace(0, block_duration, blocksize)
-    y1 = np.zeros(blocksize)
-    y2 = np.zeros(blocksize)
-    line1, = ax[0].plot(x, y1, label='15 kHz Band')
-    line2, = ax[1].plot(x, y2, label='10 kHz Band')
-    ax[0].set_ylim([-1, 1])  # Genlik ölçeklendirme
-    ax[1].set_ylim([-1, 1])  # Genlik ölçeklendirme
-    ax[0].set_xlim([0, block_duration])  # Zaman ekseni ölçeklendirme
-    ax[1].set_xlim([0, block_duration])  # Zaman ekseni ölçeklendirme
-    ax[0].legend()
-    ax[1].legend()
-    """
-
     with sd.InputStream(callback=audio_callback, channels=1, samplerate=sampling_rate, blocksize=blocksize):
         print("Mikrofon dinleniyor... 'Ctrl+C' ile çıkış yapabilirsiniz.")
         process_thread = threading.Thread(target=process_audio)
         process_thread.daemon = True
         process_thread.start()
-        """
-        plot_thread = threading.Thread(target=update_plot, args=(fig, ax, line1, line2))
-        plot_thread.daemon = True
-        plot_thread.start()
-        plot_thread.join()
-        """
         process_thread.join()
-        
 
 if __name__ == "__main__":
     listen_microphone()
