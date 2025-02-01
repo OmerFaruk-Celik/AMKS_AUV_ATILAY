1b646234202b2489c4db9650dab55c0864a0262f
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-01 14:10:01 +0300

    oto commit 2025-02-01 14:10:01.279369

--- receiver4.py
+++ receiver4.py
@@ -56,12 +56,12 @@ def process_audio():
         if not q.empty():
             indata = q.get()
             dominant_freq = find_dominant_frequency(indata[:, 0], sampling_rate)
-            #print(f"Dominant Frequency: {dominant_freq} Hz")
+            print(f"Dominant Frequency: {dominant_freq} Hz")
             is15=is15Khz(dominant_freq)
             is10=is10Khz(dominant_freq)
-            print(is15)
+            #print(is15)
             xor_or(is15,is10)
-            print(list(q2.queue)) ##Bu yorum satırlarını silme lazım olacak şekilde tekrardan kullanmak için şimdilik yorum satırına alıyorum
+            #print(list(q2.queue)) ##Bu yorum satırlarını silme lazım olacak şekilde tekrardan kullanmak için şimdilik yorum satırına alıyorum
 
 def update_plot_and_fft():
     """Bu fonksiyon grafiği günceller ve Fourier dönüşümü ile frekans analizini yapar."""
