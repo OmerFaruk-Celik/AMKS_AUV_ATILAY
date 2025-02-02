96b286a37e25d27cab84673606e269e46c7bc83f
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-02 23:00:01 +0300

    oto commit 2025-02-02 23:00:01.726197

--- receiver3.py
+++ receiver3.py
@@ -33,7 +33,7 @@ def audio_callback(indata, frames, time, status):
     if q.full():
         q.get()  # Kuyruktan fazladan verileri çıkar		
     q.put(indata.copy() * scale_factor, block=False)  # Genlik ölçekleme ekle
-    print(indata[:10])
+    #print(len(indata))
                 
 def calculate_frequency(data, sampling_rate):
     """Bu fonksiyon verilen veri için frekansı hesaplar."""
