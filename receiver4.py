98019da63376a5e3b97c3146edda010c5bdec927
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-02 14:40:01 +0300

    oto commit 2025-02-02 14:40:01.555845

--- receiver4.py
+++ receiver4.py
@@ -60,9 +60,9 @@ def process_audio():
             #print(f"Dominant Frequency: {dominant_freq} Hz")
             is18 = is18Khz(dominant_freq)
             is16 = is16Khz(dominant_freq)
-            #print(f"is18Khz: {is18}, is16Khz: {is16}")
+            print(f"is18Khz: {is18}, is16Khz: {is16}")
             xor_or(is18, is16)
-            print(list(q2.queue)) ##Bu yorum satırlarını silme! lazım olacak şekilde tekrardan kullanmak için şimdilik yorum satırına alıyorum
+            #print(list(q2.queue)) ##Bu yorum satırlarını silme! lazım olacak şekilde tekrardan kullanmak için şimdilik yorum satırına alıyorum
 
 def update_plot_and_fft():
     """Bu fonksiyon grafiği günceller ve Fourier dönüşümü ile frekans analizini yapar."""
