5e6c6cda7e59261b413020cd783a478d4a44246c
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-02 19:59:02 +0300

    oto commit 2025-02-02 19:59:02.340010

--- receiver3.py
+++ receiver3.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Sabitler
 sampling_rate = 20000  # Örnekleme frekansı (Hz)
-block_duration = 0.2  # Blok süresi (saniye)
+block_duration = 0.1  # Blok süresi (saniye)
 blocksize = int(sampling_rate * block_duration)  # Blok boyutu (örnek sayısı)
 scale_factor = 10  # Genlik ölçekleme faktörü
 tolerance = 200  # Frekans toleransı (Hz)
