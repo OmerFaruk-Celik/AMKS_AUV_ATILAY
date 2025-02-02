e19d8e5dfa53d4a8afc88c1763f3c7cad97cd6cd
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-03 01:03:01 +0300

    oto commit 2025-02-03 01:03:01.953133

--- receiver3.py
+++ receiver3.py
@@ -49,13 +49,13 @@ def update_plot():
     global say
     plt.ion()  # Interaktif modu etkinleştir
     fig, (ax1, ax2) = plt.subplots(2, 1)  # İki alt grafik oluştur
-    x = np.arange(0, 2000)  # 2000 nokta
-    y = np.zeros(2000)
+    x = np.arange(0, 1000)  # 2000 nokta
+    y = np.zeros(1000)
     line1, = ax1.plot(x, y)
     ax1.set_ylim([-0.2, 0.2])
-    ax1.set_xlim([0, 2000])
+    ax1.set_xlim([0, 1000])
     ax1.set_title("Time Domain Signal")
-    ax1.set_xticks(np.arange(0, 2001, 125))  # X ekseninde 0'dan 2000'e kadar 125'in katlarında tıklamalar ekle
+    ax1.set_xticks(np.arange(0, 1001, 125))  # X ekseninde 0'dan 2000'e kadar 125'in katlarında tıklamalar ekle
 
     freq_text1 = ax2.text(0.5, 0.5, '', transform=ax2.transAxes, ha='center')
     freq_text2 = ax2.text(0.5, 0.4, '', transform=ax2.transAxes, ha='center')
