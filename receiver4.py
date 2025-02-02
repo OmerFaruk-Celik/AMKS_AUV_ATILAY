ad90ff7f121d0662be96950142eb185a35a13a59
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-02 15:05:01 +0300

    oto commit 2025-02-02 15:05:01.468527

--- receiver4.py
+++ receiver4.py
@@ -59,7 +59,7 @@ def process_audio():
         if not q.empty():
             indata = q.get()
             dominant_freq = find_dominant_frequency(indata[:, 0], sampling_rate)
-            if dominant_freq >=14000 and dominant_freq <=19000:
+            if dominant_freq >=16000 and dominant_freq <=19000:
                 print(f"Dominant Frequency: {dominant_freq} Hz")
                 is18 = is18Khz(dominant_freq)
                 is16 = is16Khz(dominant_freq)
