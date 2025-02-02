154c51a3ba7673ecb63adca6214c6b58b1f233c5
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-02 23:40:04 +0300

    oto commit 2025-02-02 23:40:04.550860

--- receiver3.py
+++ receiver3.py
@@ -103,10 +103,11 @@ def update_plot():
                 ]
                 freqs_array = check_frequencies(freqs, 2000, tolerance)
 
-                if (freqs_array[1]) and freqs_array[2]:
+                if (freqs_array[0]) and freqs_array[1]:
                     freq_text1.set_text(f'Grup1 Frekansı: {freqs[0]:.2f} Hz')
                     freq_text2.set_text(f'Grup2 Frekansı: {freqs[1]:.2f} Hz')
                     line1.set_ydata(display_data)
+                    print(freqs_array)
                 else:
                     freq_text1.set_text('Grup1 frekansı 2kHz civarında değil.')
                     freq_text2.set_text('Grup2 frekansı 2kHz civarında değil.')
