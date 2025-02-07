7f6239acdec621e24f9000d168795b14ce007a39
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-07 15:33:01 +0300

    oto commit 2025-02-07 15:33:01.865145

--- osiloskop.py
+++ osiloskop.py
@@ -49,7 +49,7 @@ s_duration = Slider(ax_duration, 'Duration', 0.0001, 0.1, valinit=DURATION, vals
 s_sample_rate = Slider(ax_sample_rate, 'Sample Rate', 20000, 200000, valinit=SAMPLE_RATE, valstep=1000)
 s_xlim = Slider(ax_xlim, 'X Lim', 0.001, 0.1, valinit=XLIM, valstep=0.001, orientation='vertical')
 s_ylim = Slider(ax_ylim, 'Y Lim', 0.001, 1, valinit=YLIM, valstep=0.01, orientation='vertical')
-s_interval = Slider(ax_interval, 'Interval', 10, 200, valinit=INTERVAL, orientation='vertical')
+s_interval = Slider(ax_interval, 'Interval', 10, 800, valinit=INTERVAL, orientation='vertical')
 
 def init():
     ax.set_xlim(0, XLIM)
