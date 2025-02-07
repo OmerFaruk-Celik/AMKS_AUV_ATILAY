b2b3af3eef80626a6b95caa17b8b33bef1080998
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-07 18:02:01 +0300

    oto commit 2025-02-07 18:02:01.591105

--- MyTools/ornek.py
+++ MyTools/ornek.py
@@ -1,17 +1,16 @@
 from osiloskop import Osiloskop
 
-# Create an instance
+# Create and start stream
 osc = Osiloskop()
-
-# Start streaming
 osc.start_stream()
 
-# Get microphone data
-data = osc.get_data()
-
-# Send data (if needed)
-print(data)
-processed_data = osc.send_data(data)
-
-# Visualize
+# Visualize non-blockingly
 osc.visualize()
+
+# You can continue executing code here
+print("Continuing with other tasks...")
+
+# Later, process data
+raw_data = osc.get_data()
+filtered_data = osc.apply_bandpass_filter(raw_data, 20, 2000)
+dom_freq = osc.estimate_dominant_frequency(filtered_data)
