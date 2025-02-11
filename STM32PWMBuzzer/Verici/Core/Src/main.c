7c64e18bb0b86de4cee02f02eafd1cbf8b3833bf
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-02-11 21:04:01 +0300

    oto commit 2025-02-11 21:04:01.537054

--- STM32PWMBuzzer/Verici/Core/Src/main.c
+++ STM32PWMBuzzer/Verici/Core/Src/main.c
@@ -78,8 +78,8 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
         	ARR+=ekle;
 
 
-        	if(ARR>=4000 || ARR <=100){
-        		ekle=ekle*-1;
+        	if(ARR>=4000{
+        		ekle=-100;
         		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
         	}
 
