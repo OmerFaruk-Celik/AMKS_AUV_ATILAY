cf5f2f03a21767327571b5f76771d897317ead66
OmerFaruk-Celik <faruktrhakan@hotmail.com>
2025-03-19 12:43:01 +0300

    oto commit 2025-03-19 12:43:01.340551

--- receiver1.0.py
+++ receiver1.0.py
@@ -148,7 +148,7 @@ def plot_recorded_signal(recorded_signal, sampling_rate):
 while True:
 	
     # Gelen sinyali dinle ve decode et
-    bit_array,v2= listen_and_decode(duration_per_bit=0.01, num_bits=8, sampling_rate=200000)
+    bit_array,v2= listen_and_decode(duration_per_bit=0.001, num_bits=8, sampling_rate=200000)
     hamming_array=hamming_8_4_decode(bit_array)
     #print("Alınan Bit Dizisi:", bit_array)
     binary_array = gray_to_bin(hamming_array)
